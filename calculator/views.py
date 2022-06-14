12094dd4399ce4472e6301edcba9030a348b8611
drfoehn <janne.unger@gmail.com>
2022-06-14 09:14:19 +0200

    bokeh installed

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,4 @@
+from bokeh.models import Range1d
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.messages.views import SuccessMessageMixin
@@ -7,6 +8,8 @@ from django.core import validators
 from .forms import *
 from django.shortcuts import redirect, render
 from .models import *
+from bokeh.plotting import figure, show, output_file
+from bokeh.embed import components
 
 class ResultsView(DetailView):
     template_name = 'calculator/results.html'
@@ -16,25 +19,60 @@ class ResultsView(DetailView):
     # extra_context={'subjects': Subject.objects.all()}, {'results': Result.objects.all()}
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         subjects = Subject.objects.filter(setting=self.object)
-        """
-        subject1
-            -replicate1
-                -result1[2h]
-                -result2[4h]
-            -repicate2
-                -result3[2h]
-                -result4[4h]
-            
-        """
+
+
+        # average = Subject.average(subjects, Duration)
+
+        # create some data
+        x1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        y1 = [0, 8, 2, 4, 6, 9, 5, 6, 25, 28, 4, 7]
+        x2 = [2, 5, 7, 15, 18, 19, 25, 28, 9, 10, 4]
+        y2 = [2, 4, 6, 9, 15, 18, 0, 8, 2, 25, 28]
+        x3 = [0, 1, 0, 8, 2, 4, 6, 9, 7, 8, 9]
+        y3 = [0, 8, 4, 6, 9, 15, 18, 19, 19, 25, 28]
+
+        # select the tools you want
+        TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+
+        # the red and blue graphs share this data range
+        xr1 = Range1d(start=0, end=30)
+        yr1 = Range1d(start=0, end=30)
+
+        # only the green graph uses this data range
+        xr2 = Range1d(start=0, end=30)
+        yr2 = Range1d(start=0, end=30)
+
+        # build the figures
+        p1 = figure(x_range=xr1, y_range=yr1, tools=TOOLS, width=300, height=300)
+        p1.scatter(x1, y1, size=12, color="red", alpha=0.5)
+
+        p2 = figure(x_range=xr1, y_range=yr1, tools=TOOLS, width=300, height=300)
+        p2.scatter(x2, y2, size=12, color="blue", alpha=0.5)
+
+        p3 = figure(x_range=xr2, y_range=yr2, tools=TOOLS, width=300, height=300)
+        p3.scatter(x3, y3, size=12, color="green", alpha=0.5)
+
+        # plots can be a single Bokeh model, a list/tuple, or even a dictionary
+        plots = {'Red': p1, 'Blue': p2, 'Green': p3}
+
+        script, div = components(plots)
+
+
+
         context["results"] = Result.objects.all()
         context["durations"] = Duration.objects.all()
         context["replicates"] = Replicate.objects.all()
         context["subjects"] = subjects
+        context["div"] = div
+        context["script"] = script
+
+
+
+
+
+
         return context
-
-
 
 class DashboardView(ListView):
     model = Instrument
