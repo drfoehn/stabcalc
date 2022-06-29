6e6bd723b24a5c5ce4bb6d5959227a6388ade861
drfoehn <janne.unger@gmail.com>
2022-06-29 12:22:49 +0200

    list/add results working

--- calculator/views.py
+++ calculator/views.py
@@ -833,43 +833,63 @@ def delete_duration(request, pk):
 
 # -------------------------------------RESULTS---------------------------------
 
+# def create_result(request, setting_pk):
+#     setting = Setting.objects.get(pk=setting_pk)
+#     durations = Duration.objects.filter(setting=setting)
+#     subjects = Subject.objects.filter(setting=setting)
+#     results = Result.objects.filter(setting=setting)
+#
+#     ResultFormSet = modelformset_factory(Result, fields=('value', 'setting', 'replicate', 'duration', 'subject'),
+#                                          extra=1)
+#
+#     if request.method == 'POST':
+#         formset = ResultFormSet(request.POST or None)
+#
+#         if formset.is_valid():
+#
+#             results = formset.save()
+#             # TODO: Show success message after save that fades automatically: https://stackoverflow.com/questions/61153261/make-success-message-disappear-after-few-seconds-of-django-form-submission-and-d
+#             # messages.add_message(request, messages.SUCCESS, 'Entry saved')
+#             # for result in results:
+#             #     sid=result.setting.id
+#             # # return redirect('result-detail', pk=rid)
+#             # return redirect('create-result', setting_pk=sid)
+#
+#         else:
+#             context = {
+#                 'formset': formset,
+#                 'results': results
+#             }
+#             return render(request, 'calculator/partials/result_form.html', context)
+#     formset = ResultFormSet(queryset=Result.objects.none())
+#
+#     context = {
+#         'formset': formset,
+#         'durations': durations,
+#         'setting': setting,
+#         'subjects': subjects,
+#         'results': results,
+#     }
+#
+#     return render(request, 'calculator/result_list.html', context)
 def create_result(request, setting_pk):
+    form = ResultForm(request.POST or None)
     setting = Setting.objects.get(pk=setting_pk)
-    durations = Duration.objects.filter(setting=setting_pk)
-    subjects = Subject.objects.filter(setting=setting_pk)
-
-    results = Result.objects.filter(setting=setting_pk)
-    # sub_id = []
-    # for subject in subjects.all():
-    #     sub_id.append(subject.id)
-    #
-    # res_sub_id = []
-    # for result in results:
-    #     res_sub_id.append(result._get_pk_val(subject))
-    ResultFormSet = modelformset_factory(Result, fields=('value', 'setting', 'replicate', 'duration', 'subject'),
-                                         extra=1)
-    # formset = ResultFormSet(queryset=Result.objects.none())
+    durations = Duration.objects.filter(setting=setting)
+    subjects = Subject.objects.filter(setting=setting)
+    results = Result.objects.filter(setting=setting)
 
     if request.method == 'POST':
-        formset = ResultFormSet(request.POST or None)
-
-        if formset.is_valid():
-
-            results = formset.save()
-            # TODO: Show success message after save that fades automatically: https://stackoverflow.com/questions/61153261/make-success-message-disappear-after-few-seconds-of-django-form-submission-and-d
-            # messages.add_message(request, messages.SUCCESS, 'Entry saved')
-            for result in results:
-                sid=result.setting.id
-            # return redirect('result-detail', pk=rid)
-            return redirect('create-result', setting_pk=sid)
-
+        if form.is_valid():
+            result = form.save()
+            return redirect('result-detail', pk=result.id)
+        # FIXME: Redirect not correct
         else:
             context = {
-                'form': formset,
+                'form': form,
                 'results': results
             }
             return render(request, 'calculator/partials/result_form.html', context)
-    form = ResultFormSet(queryset=Result.objects.none())
 
     context = {
         'form': form,
@@ -877,21 +897,18 @@ def create_result(request, setting_pk):
         'setting': setting,
         'subjects': subjects,
         'results': results,
-
     }
-
 
     return render(request, 'calculator/result_list.html', context)
 
-
-
 def add_result_form(request):
-    ResultFormSet = modelformset_factory(Result, fields=('value','setting', 'replicate', 'duration', 'subject'), extra=1)
-    form = ResultFormSet(queryset=Result.objects.none())
+    form = ResultForm()
     context = {
-        'form': form,
+        "form": form
     }
     return render(request, 'calculator/partials/result_form.html', context)
+
+
 
 # def add_result_form(request):
 #     form = ResultForm()
@@ -914,11 +931,11 @@ def result_detail(request, pk):
 def edit_result(request, pk):
     obj = Result.objects.get(pk=pk)
     ResultFormSet = modelformset_factory(Result, fields=('value','setting', 'replicate', 'duration', 'subject'))
-    form = ResultFormSet(request.POST or None, queryset=obj)
-    if form.is_valid():
-        form.save()
+    formset = ResultFormSet(request.POST or None, queryset=obj)
+    if formset.is_valid():
+        formset.save()
     context = {
-        'form': form
+        'formset': formset
     }
     return render(request, 'calculator/partials/result_form.html', context)
 
@@ -1017,54 +1034,54 @@ def delete_subject(request, pk):
 
 
 
-class InstrumentUpdateView(UpdateView):
-    model = Instrument
-    form_class = InstrumentForm
-    # template_name = 'instrument_update.html'  # templete for updating
-    success_url = "/dashboard"
+# class InstrumentUpdateView(UpdateView):
+#     model = Instrument
+#     form_class = InstrumentForm
+#     # template_name = 'instrument_update.html'  # templete for updating
+#     success_url = "/dashboard"
 
 
-class SampleAddView(CreateView):
-    # template_name = "xxx.html"
-    model = Sample
-    form_class = SampleForm
-    success_url = "/dashboard"
-
-    def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
-
-
-class SettingAddView(CreateView):
-    template_name = "calculator/result_form.html"
-    model = Setting
-    form_class = SettingForm
-    # success_url = "/dashboard"
+# class SampleAddView(CreateView):
+#     # template_name = "xxx.html"
+#     model = Sample
+#     form_class = SampleForm
+#     success_url = "/dashboard"
+#
+#     def post(self, request, *args, **kwargs):
+#         return super().post(request, *args, **kwargs)
 
 
-class ValuesAddView(TemplateView):
-    template_name = "calculator/result_form.html"
+# class SettingAddView(CreateView):
+#     template_name = "calculator/result_form.html"
+#     model = Setting
+#     form_class = SettingForm
+#     # success_url = "/dashboard"
 
-    # model = Result
-    # form_class = ResultForm
-    # success_url = "/dashboard"
 
-    # Define method to handle GET request
-    def get(self, *args, **kwargs):
-        # Create an instance of the formset
-        formset = ValueFormset(queryset=Result.objects.none())
-        return self.render_to_response({"value_formset": formset})
-
-    def post(self, *args, **kwargs):
-        formset = ValueFormset(data=self.request.POST)
-        # Check if submitted forms are valid
-        if formset.is_valid():
-            formset.save()
-            return redirect(reverse_lazy("add_results"))
-
-        return self.render_to_response({"value_formset": formset})
-
-    # def post(self, request, *args, **kwargs):
-    #     return super().post(request, *args, **kwargs)
+# class ValuesAddView(TemplateView):
+#     template_name = "calculator/result_form.html"
+#
+#     # model = Result
+#     # form_class = ResultForm
+#     # success_url = "/dashboard"
+#
+#     # Define method to handle GET request
+#     def get(self, *args, **kwargs):
+#         # Create an instance of the formset
+#         formset = ValueFormset(queryset=Result.objects.none())
+#         return self.render_to_response({"value_formset": formset})
+#
+#     def post(self, *args, **kwargs):
+#         formset = ValueFormset(data=self.request.POST)
+#         # Check if submitted forms are valid
+#         if formset.is_valid():
+#             formset.save()
+#             return redirect(reverse_lazy("add_results"))
+#
+#         return self.render_to_response({"value_formset": formset})
+#
+#     # def post(self, request, *args, **kwargs):
+#     #     return super().post(request, *args, **kwargs)
 
 
 # def result_view(request):
@@ -1083,79 +1100,79 @@ class ValuesAddView(TemplateView):
 #         return render(request,'calculator/result_form.html',{'formset':formset})
 
 
-class ParameterIndex(ListView):
-    model = Parameter
-
-
-class ParameterDetail(DetailView):
-    model = Parameter
-
-
-class ParameterAddView(CreateView):
-    # template_name = "xxx.html"
-    model = Parameter
-    form_class = ParameterForm
-
-    def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
-
-
-class MultiInputView(TemplateView):
-    ### TemplateResponseMixin
-    template_name = "calculator/calculator_form.html"
-
-    ### ContextMixin
-    def get_context_data(self, **kwargs):
-        """Adds extra content to our template"""
-        context = super(MultiInputView, self).get_context_data(**kwargs)
-
-        context["setting_form"] = SettingForm(
-            prefix="SettingForm",
-            # Multiple 'submit' button paths should be handled in form's .save()/clean()
-            data=self.request.POST
-            if bool(
-                set(
-                    [
-                        "SettingForm-submit",
-                    ]
-                ).intersection(self.request.POST)
-            )
-            else None,
-        )
-
-        context["duration_form"] = DurationForm(
-            prefix="duration",
-            data=self.request.POST if "Duration-submit" in self.request.POST else None,
-            files=self.request.FILES
-            if "Duration-submit" in self.request.POST
-            else None,
-        )
-
-        context["value_form"] = ValueForm(
-            prefix="value",
-            data=self.request.POST if "Value-submit" in self.request.POST else None,
-            files=self.request.FILES if "Value-submit" in self.request.POST else None,
-        )
-
-        # context['value_form'] = ValueForm()
-        return context
-
-    ### NegotiationGroupDetailView
-    def post(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-
-        if context["setting_form"].is_valid():
-            instance = context["setting_form"].save()
-            # messages.success(request, 'Setting saved.'.format(instance.pk))
-        elif context["duration_form"].is_valid():
-            instance = context["parameter_form"].save()
-            # messages.success(request, 'Duration setting has been saved.'.format(instance.pk))
-        elif context["value_form"].is_valid():
-            instance = context["value_form"].save()
-            # messages.success(request, 'Value has been saved.'.format(instance.pk))
-            # advise of any errors
-
-        else:
-            # messages.error('Error(s) encountered during form processing, please review below and re-submit')
-            pass
-        return self.render_to_response(context)
+# class ParameterIndex(ListView):
+#     model = Parameter
+#
+#
+# class ParameterDetail(DetailView):
+#     model = Parameter
+#
+#
+# class ParameterAddView(CreateView):
+#     # template_name = "xxx.html"
+#     model = Parameter
+#     form_class = ParameterForm
+#
+#     def post(self, request, *args, **kwargs):
+#         return super().post(request, *args, **kwargs)
+#
+#
+# class MultiInputView(TemplateView):
+#     ### TemplateResponseMixin
+#     template_name = "calculator/calculator_form.html"
+#
+#     ### ContextMixin
+#     def get_context_data(self, **kwargs):
+#         """Adds extra content to our template"""
+#         context = super(MultiInputView, self).get_context_data(**kwargs)
+#
+#         context["setting_form"] = SettingForm(
+#             prefix="SettingForm",
+#             # Multiple 'submit' button paths should be handled in form's .save()/clean()
+#             data=self.request.POST
+#             if bool(
+#                 set(
+#                     [
+#                         "SettingForm-submit",
+#                     ]
+#                 ).intersection(self.request.POST)
+#             )
+#             else None,
+#         )
+#
+#         context["duration_form"] = DurationForm(
+#             prefix="duration",
+#             data=self.request.POST if "Duration-submit" in self.request.POST else None,
+#             files=self.request.FILES
+#             if "Duration-submit" in self.request.POST
+#             else None,
+#         )
+#
+#         context["value_form"] = ValueForm(
+#             prefix="value",
+#             data=self.request.POST if "Value-submit" in self.request.POST else None,
+#             files=self.request.FILES if "Value-submit" in self.request.POST else None,
+#         )
+#
+#         # context['value_form'] = ValueForm()
+#         return context
+#
+#     ### NegotiationGroupDetailView
+#     def post(self, request, *args, **kwargs):
+#         context = self.get_context_data(**kwargs)
+#
+#         if context["setting_form"].is_valid():
+#             instance = context["setting_form"].save()
+#             # messages.success(request, 'Setting saved.'.format(instance.pk))
+#         elif context["duration_form"].is_valid():
+#             instance = context["parameter_form"].save()
+#             # messages.success(request, 'Duration setting has been saved.'.format(instance.pk))
+#         elif context["value_form"].is_valid():
+#             instance = context["value_form"].save()
+#             # messages.success(request, 'Value has been saved.'.format(instance.pk))
+#             # advise of any errors
+#
+#         else:
+#             # messages.error('Error(s) encountered during form processing, please review below and re-submit')
+#             pass
+#         return self.render_to_response(context)
